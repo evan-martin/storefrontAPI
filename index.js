d7ceb35928e5b8ecac8496ca1d4c5dba750349ad
evan <evanm689@gmail.com>
2022-07-06 13:45:49 -0700

    cors fixes

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.post('/create-checkout-session', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS');
   const session = await stripe.checkout.sessions.create({
     line_items: 
       req.body.payload,
